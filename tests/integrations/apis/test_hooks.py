dec24051b01708b1c28d7830e8255069337cb05f
Oleg Makarov <imflop@gmail.com>
2024-04-14 12:14:11 +0300

    More deep test

--- tests/integrations/apis/test_hooks.py
+++ tests/integrations/apis/test_hooks.py
@@ -1,9 +1,9 @@
-import json
 from unittest import mock
 
 import pytest
 
-from brew_scout.libs.services.bus.service import BusService
+from brew_scout.libs.domains.telegram import TelegramMethods
+from brew_scout.libs.services.bus.client import TelegramClient
 
 
 @pytest.fixture()
@@ -34,12 +34,21 @@ def payload(faker):
     }
 
 
-async def test_handle_telegram_hook(client, caplog, payload):
-    with mock.patch.object(BusService, "send_city_not_found_message", return_value=None) as mocked:
+async def test_handle_telegram_hook_if_city_not_found(client, caplog, payload):
+    with mock.patch.object(TelegramClient, "post", return_value=None) as mocked:
         res = await client.post("/api/v1/hook/telegram?run_now=1", json=payload)
 
+    latitude, longitude = payload["message"]["location"]["latitude"], payload["message"]["location"]["longitude"]
+
     assert res.status_code == 204
-    assert [
-        f'City not found with given coordinates: {payload["message"]["location"]["latitude"]} {payload["message"]["location"]["longitude"]}'
-    ] == [record.message for record in caplog.records if record.funcName == "process_hook"]
-    mocked.assert_called_once_with(payload["message"]["chat"]["id"])
+    assert [f"City not found with given coordinates: {latitude} {longitude}"] == [
+        record.message for record in caplog.records if record.funcName == "process_hook"
+    ]
+    mocked.assert_called_once_with(
+        TelegramMethods.SEND_MESSAGE,
+        {
+            "chat_id": payload["message"]["chat"]["id"],
+            "text": "Sorry, your city has not been added yet.",
+            "parse_mode": "html",
+        },
+    )
